c30699169bec639aa6f44c334dd58739385fe8e3
Mitar Perovic <perovicmitar@uns.ac.rs>
2021-12-11 17:34:05 +0100

    ws fix

--- order/consumers/order_status_consumer.py
+++ order/consumers/order_status_consumer.py
@@ -9,7 +9,7 @@ class OrderStatusConsumer(AsyncJsonWebsocketConsumer):
 
     async def connect(self):
 
-        order_id: int = self.scope['url_route']['kwargs']['order_id']
+        order_id: str = self.scope['url_route']['kwargs']['order_id']
         self.order_group = 'order_' + order_id
         await self.accept()
         await self.channel_layer.group_add(self.order_group, self.channel_name)
@@ -19,13 +19,19 @@ class OrderStatusConsumer(AsyncJsonWebsocketConsumer):
         print(data)
         self.send_json(data)
 
-    def disconnect(self, code):
-        async_to_sync(self.channel_layer.group_discard)(
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard(
             self.order_group,
             self.channel_name
         )
+        await self.close()
 
     async def forward_group_message(self, event):
         await self.send(json.dumps(event['data'], default=str))
+
+    async def send_to_group(self, message):
+        return self.channel_layer.group_send(self.order_group,
+                                                            {"type": "forward.group.message",
+                                                             "data": message})
 
 
