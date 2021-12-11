98c07641fa09c1ddec50d7747ea59e3561c1edc5
Mitar Perovic <perovicmitar@uns.ac.rs>
2021-12-11 16:41:33 +0100

    modified image model

--- image/models.py
+++ image/models.py
@@ -5,3 +5,6 @@ from shared.models.soft_deletable_model import SoftDeletableModel
 class Image(SoftDeletableModel):
     caption = models.TextField()
     file = models.ImageField(upload_to='images', default='default.jpg')
+
+    def __str__(self):
+        return self.caption
